6408cd755fca364bd021e488fccf51a93c0786cc
Ville Laaksonen <leligens@gmail.com>
2022-09-16 15:08:25 +0300

    new pages + moved pages to components folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import Register from "./Register";
-import Login from "./Login";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Layout from "./components/Layout";
+import Admin from "./components/Admin";
 
 function App() {
-
     return (
         <main className="App">
             <Login />
