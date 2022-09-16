65f51de0e072a02da2765491080ee0a56c2609fe
Ville Laaksonen <leligens@gmail.com>
2022-09-16 15:47:13 +0300

    page content for testing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Unauthorized from "./components/Unauthorized";
 import Missing from "./components/Missing";
 import Admin from "./components/Admin";
 import LinkPage from "./components/LinkPage";
+
+import RequireAuth from "./components/RequireAuth";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,17 +15,18 @@ function App() {
         <Routes>
             <Route path="/" element={<Layout />}>
                 {/* public routes */}
-                <Route path="login" element={<Login />}/>
-                <Route path="register" element={<Register />}/>
-                <Route path="linkpage" element={<LinkPage />}/>
-                <Route path="unauthorized" element={<Unauthorized />}/>
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="linkpage" element={<LinkPage />} />
+                <Route path="unauthorized" element={<Unauthorized />} />
 
                 {/* protected routes */}
-                <Route path="/" element={<Home />}/>
-                <Route path="admin" element={<Admin />}/>
-                
+                <Route element={<RequireAuth />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="admin" element={<Admin />} />
+                </Route>
                 {/* catch all */}
-                <Route path="*" element={<Missing />}/>
+                <Route path="*" element={<Missing />} />
             </Route>
         </Routes>
     )
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,22 @@
 import { useRef, useState, useEffect, useContext } from "react";
-import AuthContext from "../context/AuthProvider";
-
-import axios from "../api/axios"
+import useAuth from "../hooks/useAuth";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import axios from "../api/axios";
 const LOGIN_URL = "/auth";
 
 const Login = () => {
-    const { setAuth } = useContext(AuthContext);
+    const { setAuth } = useAuth();
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/";
+
     const userRef = useRef();
     const errRef = useRef();
 
     const [user, setUser] = useState("");
     const [pwd, setPwd] = useState("");
     const [errMsg, setErrMsg] = useState("");
-    const [success, setSuccess] = useState(false);
 
     useEffect(() => {
         userRef.current.focus();
@@ -27,9 +31,9 @@ const Login = () => {
 
         try {
             const response = await axios.post(LOGIN_URL,
-                JSON.stringify({ player: user, pwd}),
+                JSON.stringify({ player: user, pwd }),
                 {
-                    headers: { "Content-Type": "application/json"},
+                    headers: { "Content-Type": "application/json" },
                     withCredentials: true
                 }
             );
@@ -44,8 +48,10 @@ const Login = () => {
             setSuccess(true);
 
             setAuth({ user, pwd, roles, accessToken });
+
+            navigate(from, { replace: true });
         } catch (err) {
-            
+
             if (err?.response?.status === 0) {
                 setErrMsg("No server response");
             } else if (err.response?.status === 400) {
@@ -60,51 +66,42 @@ const Login = () => {
     }
 
     return (
-        <>
-            {success ? (
-                <section>
-                    <h1>You are logged in!</h1>
-                    <p>
-                        <a href="#">Go to Home</a>
-                    </p>
-                </section>
-            ) : (
-                <section>
-                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-                    <h1>Sign In</h1>
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="username">Username:</label>
-                        <input
-                            type="text"
-                            id="username"
-                            ref={userRef}
-                            autoComplete="off"
-                            onChange={(e) => setUser(e.target.value)}
-                            value={user}
-                            required
-                        />
 
-                        <label htmlFor="password">Password:</label>
-                        <input
-                            type="password"
-                            id="password"
-                            onChange={(e) => setPwd(e.target.value)}
-                            value={pwd}
-                            required
-                        />
+        <section>
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+            <h1>Sign In</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    id="username"
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(e) => setUser(e.target.value)}
+                    value={user}
+                    required
+                />
 
-                        <button>Sign In</button>
-                    </form>
-                    <p>
-                        Need an Account?<br />
-                        <span className="line">
-                            {/* Router link here */}
-                            <a href="#">Sign Up</a>
-                        </span>
-                    </p>
-                </section>
-            )}
-        </>
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    onChange={(e) => setPwd(e.target.value)}
+                    value={pwd}
+                    required
+                />
+
+                <button>Sign In</button>
+            </form>
+            <p>
+                Need an Account?<br />
+                <span className="line">
+                    {/* Router link here */}
+                    <a href="#">Sign Up</a>
+                </span>
+            </p>
+        </section>
+
     )
 }
 
