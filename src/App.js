53aea168480da4966a21d0be217ee40b7cc26db8
Ville Laaksonen <leligens@gmail.com>
2022-09-16 15:25:23 +0300

    set up routes in App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Layout from "./components/Layout";
+import Unauthorized from "./components/Unauthorized";
+import Missing from "./components/Missing";
 import Admin from "./components/Admin";
+import LinkPage from "./components/LinkPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <main className="App">
-            <Login />
-        </main>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                {/* public routes */}
+                <Route path="login" element={<Login />}/>
+                <Route path="register" element={<Register />}/>
+                <Route path="linkpage" element={<LinkPage />}/>
+                <Route path="unauthorized" element={<Unauthorized />}/>
+
+                {/* protected routes */}
+                <Route path="/" element={<Home />}/>
+                <Route path="admin" element={<Admin />}/>
+                
+                {/* catch all */}
+                <Route path="*" element={<Missing />}/>
+            </Route>
+        </Routes>
     )
 }
 
