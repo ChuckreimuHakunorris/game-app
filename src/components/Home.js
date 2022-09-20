2946dc2cb950e66d41427f6f62ac328104bc2889
Ville Laaksonen <leligens@gmail.com>
2022-09-20 17:21:51 +0300

    show logged in users username

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
 import { useNavigate, Link } from "react-router-dom";
 import useLogout from "../hooks/useLogout";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const Home = () => {
     const navigate = useNavigate();
     const logout = useLogout();
+
+    const value = useLocalStorage("user");
 
     const signOut = async () => {
         await logout();
@@ -14,7 +17,7 @@ const Home = () => {
         <section>
             <h1>Home</h1>
             <br />
-            <p>You are logged in!</p>
+            <p>You are logged in as {value}</p>
             <br />
             <Link to="/admin">Go to the Admin page</Link>
             <br />
