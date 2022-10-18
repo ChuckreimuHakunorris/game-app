43529c54a8c6329d89a0c61ed536459d2fb30933
Ville Laaksonen <leligens@gmail.com>
2022-10-18 17:02:59 +0300

    show logged in username on top bar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,11 +1,24 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import useLocalStorage from "../hooks/useLocalStorage";
+import useAuth from "../hooks/useAuth";
+import jwtDecode from "jwt-decode";
 
 const TopBar = () => {
+    const [user] = useLocalStorage("user");
+    const { auth } = useAuth();
+
+    const decoded = auth?.accessToken
+        ? jwtDecode(auth.accessToken)
+        : undefined;
+
     return (
         <div className="topnav">
             <Link to="/" id="title">Castrum Tactics</Link>
             <Link to="/game">Game</Link>
             <Link to="/admin">Admin</Link>
+            {decoded
+                ? (<div id="barUsername">{user}</div>)
+                : (<Link id="barUsername" to="/login">Login</Link>)}
         </div>
     )
 }
