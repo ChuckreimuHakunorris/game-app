9703df67c483ab2e89ffcb895dc92b482a83c182
Ville Laaksonen <leligens@gmail.com>
2022-11-08 10:14:33 +0200

    home page base

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,9 @@
-import { useNavigate, Link } from "react-router-dom";
-import useLogout from "../hooks/useLogout";
-import useLocalStorage from "../hooks/useLocalStorage";
-
 const Home = () => {
-    const navigate = useNavigate();
-    const logout = useLogout();
-
-    const [value] = useLocalStorage("user");
-
-    const signOut = async () => {
-        await logout();
-        navigate('/linkpage');
-    }
-
     return (
         <section>
-            <h1>Home</h1>
-            <br />
-            <p>You are logged in as { value }</p>
-            <br />
-            <Link to="/admin">Go to the Admin page</Link>
-            <br />
-            <Link to="/linkpage">Go to the link page</Link>
-            <br />
-            <div className="flexGrow">
-                <button onClick={signOut}>Sign Out</button>
-            </div>
+            <h1>Castrum Tactics</h1>
+            <br/>
+            <p>Game description here.</p>
         </section>
     )
 }
