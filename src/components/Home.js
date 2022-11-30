41e4703b1403d0322dc635fc0d3b2f8757e1b982
Ville Laaksonen <leligens@gmail.com>
2022-11-30 17:54:01 +0200

    finished gameplay guide

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,8 @@ const Home = () => {
             <br />
             <p>Castrum Tactics is an exciting strategy game where a good game plan and
                 reading your opponents mindset are key to victory. If you're new here, this
-                page will serve as a guide to get you started!
+                page will serve as a guide to get you started! Please note that an account is required
+                to play the game.
             </p>
             <br />
             <h3>Overview</h3>
@@ -108,7 +109,54 @@ const Home = () => {
             <br />
             <p><b>Case 3:</b> When playing into a square containing your own knight, that knight is upgraded
             into a castle. The opposing player can't conquer squares that contain your castles. Likewise, you
-            can't place a knight on top of an opponents castle.</p>
+            can't place a knight on top of an opponent's castle.</p>
+            <br />
+            <h3>When Both Players Play into the Same Square</h3>
+            <br />
+            <p>An important special case occurs when both players choose the same square. When this
+                happens, the result is always a grave, regardless of what was in the square beforehand.
+                Use this to your advantage by preventing the opponent from upgrading their knights
+                into castles at advantageous places.
+            </p>
+            <br />
+            <div className="examplePicContainer">
+                <img src={"/img/move4_1.png"} alt="missing" />
+                <img src={"/img/arrow.png"} className="arrow" alt="missing" />
+                <img src={"/img/move4_2.png"} alt="missing" />
+            </div>
+            <br />
+            <p>Here, the red player attempted to upgrade the knight in the middle square into a castle,
+                but was prevented doing so by the blue player selecting the same square.
+            </p>
+            <br />
+            <h3>Ground Health</h3>
+            <br />
+            <p>Every square on the board has an initial health value of 3. Every time a square
+                is conquered from the other player, it's ground health drops by one. This is indicated
+                by a change in color.
+            </p>
+            <br />
+            <div className="examplePicContainer">
+                <img src={"/img/groundHealth.png"} alt="missing" />
+            </div>
+            <p>Here you can see the colors and the health states they represent. When a square with health
+                one is conquered, instead of the attacking player gaining control of the square, a grave is
+                spawned as it's health drops to zero. Tiles with ground health zero will always contain a grave.
+                Therefore it is less advantageous to attack squares with health one than those of two or three,
+                as you can't gain new area this way.
+            </p>
+            <br />
+            <h3>End of the Game</h3>
+            <br />
+            <div className="examplePicContainer">
+                <img src={"/img/example4.png"} alt="missing" />
+            </div>
+            <br />
+            <p>When there are no more available moves for either player, the game is finished. After this,
+                the final count of occupied squares is tallied up and the winner (or a draw) determined. Note that
+                in the above example, one blue knight is present. This tile is completely inaccessible for the red player
+                so it's determined as belonging conclusively to the blue player.
+            </p>
         </section>
     )
 }
